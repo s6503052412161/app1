17496be2037941049adc1ca2f9841540994e5c0d
OAsiTz <s6503052412161@kmutnb.ac.th>
2023-12-25 11:17:30 +0700

    update React app.js CSS inline
    update React app.js CSS inline

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import logo from './logo_kmutnb.png';
-function App() {
-  let r = '<b>React</b>'
-  let rn = 'React Native'
-  const el=(
-    <div>
-      Create Web & Moblie App<br/>
-      wiht <span dangerouslySetInnerHTML={{__html: r}}/> & {rn}
-    </div>
-   )
-  return el 
+import './index.css'
 
+function App() {
+ const divStyle ={
+    color:'#ffff',
+    backgroundColor:'powderblue',
+    fontSize: 'larger',
+    padding:'10px',
+ }
+ return <div style={divStyle}>Hello React</div>
 }
 
 
