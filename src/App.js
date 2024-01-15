35674ebdb8b48325700fae1ad1537934177072fc
s6503052412161 <s6503052412161@kmutnb.ac.th>
2024-01-15 10:39:16 +0700

    add Calculator Function
    add Calculator Function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Header,Content,Footer} from './func-components';
 import Calendar from './class-components';
 import Button from './class-components2';
  //import './index.css';
+import { Calculator } from './calculator';
 function App() {
   return(
     <>
@@ -12,6 +13,9 @@ function App() {
     <p><center><Calendar/></center></p>
     <Content/>
     <center><Button/></center>
+    <br/>
+    <br/>
+    <Calculator/>
     <br/>
     <br/>
     <Footer/>
