7de79914e33206baefffd9df6dd427b308fb4475
s6503052412161 <s6503052412161@kmutnb.ac.th>
2024-01-15 10:13:25 +0700

    Update
    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Header,Content,Footer} from './func-components';
 import Calendar from './class-components';
 import Button from './class-components2';
-// import './sty.css';
+ //import './index.css';
 function App() {
   return(
     <>
