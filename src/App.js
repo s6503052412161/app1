2afb2af10a8ea5304be48000b9d09a1421ab3e24
s6503052412161 <s6503052412161@kmutnb.ac.th>
2024-01-29 09:04:57 +0700

    update
    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Button from './class-components2';
 import { EventData } from './event-data';
 import { Calculator } from './calculator';
 import { Table } from './event-data2';
+import {  RefsFunc } from './refs-func.js'; //ถ้าส่งออกแบบ Defalt ไม่ต้องมีปีกกา
+import RefsArray from './refs-array.js';
+import MessageBox from './state-class.js';
 function App() {
   return(
     <>
@@ -28,7 +31,18 @@ function App() {
     <Table/>
     <br/>
     <hr/>
-    
+    <h2 style={{textAlign:"center"}}>Refs function</h2> 
+    <RefsFunc/>
+    <br/>
+    <hr/>
+    <h2 style={{textAlign:"center"}}>Refs Array</h2>
+    <RefsArray/>
+    <br/>
+    <hr/>
+    <h2 style={{textAlign:"center"}}>MessageBox Add and Reduce TextSize</h2>
+    <MessageBox/>
+    <br/>
+    <hr/>
     <Footer/>
     </>
     
