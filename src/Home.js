8659aa952b655caeec7dcc665dfdea34ed4fe1ce
s6503052412161 <s6503052412161@kmutnb.ac.th>
2024-02-16 15:14:08 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home(){
     return(
         <p style={{textAlign:'center'}}>
-            <h3></h3>
+            <h3>Welcome to React Store</h3>
             Name<a href="/products">Products</a>We have to sale
             <br/>
             But your should be <a href="/member">Member</a>to buy them<br/>
