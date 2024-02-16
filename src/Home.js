97dfc2bc4bbcb1b729b9aee0901902814d25d664
s6503052412161 <s6503052412161@kmutnb.ac.th>
2024-02-16 15:13:34 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ export default function Home(){
     return(
         <p style={{textAlign:'center'}}>
             <h3></h3>
-            name<a href="/products">Products</a>We have to sale
+            Name<a href="/products">Products</a>We have to sale
             <br/>
-            But your should be <a href="/member">Member</a>to buy <br/>
+            But your should be <a href="/member">Member</a>to buy them<br/>
             if you have a Question <a href="/contact">Contact Us</a>
         </p>
     )
